ec843595dffd0d8a2008e02c30be741ae843c4c6
noiioiu <noiioiu@cocaine.ninja>
2026-01-15 12:37:29 -0600

    update language

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE IncoherentInstances #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE QualifiedDo #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE UndecidableInstances #-}
